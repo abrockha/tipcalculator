b538c7cd5ee6ac82cdd74290abd8413e44457c45
adambrockhaus <adam.m.brockhaus@gmail.com>
2021-06-13 13:59:12 -0500

    Added meat choices and list showing updated total

--- assets/scripts/tipcalculator.js
+++ assets/scripts/tipcalculator.js
@@ -2,32 +2,57 @@
 
 let taco = {
     price:[
-        3.00,
+        4.00,
         2.50,
         3.50,
-        4.00,
+        3.00,
         3.00
     ],
     meat:[
-        "chicken",
-        "ground beef",
-        "steak",
-        "shrimp",
-        "fish"
+        "Shrimp",
+        "Ground Beef",
+        "Steak",
+        "Fish",
+        "Chicken"
     ]
 };
+let preTip = 0;
+let newButton;
 
 
-function calcTip(){
+function calcPreTotal(){
     let numOfTacos = document.getElementById("numOfTacos").value;
+    let meatSelection = document.querySelector("input[name=meat]:checked").value;
+    let price = taco.price[meatSelection];
+    let preTotal = price * numOfTacos;
+    let meat = taco.meat[meatSelection];
+    let newLI = document.createElement('li');
+    newButton = document.createElement('BUTTON');
+    newButton.innerHTML = "Remove";
+
+    newLI.innerHTML = (numOfTacos + " " + meat + " tacos: " + preTotal);
+    newLI.appendChild(newButton);
+    document.getElementById("orderlist").appendChild(newLI);
+    newButton.onclick = function(){
+        preTip -= preTotal;
+        this.parentElement.remove();
+        document.getElementById('preTip').innerHTML = "Pre-Total: $" + preTip;
+    }
+    preTip += preTotal;
+    document.getElementById('preTip').innerHTML = "Pre-Total: $" + preTip;
+    console.log(preTip);
+    return preTip;
+}
+
+function calcTip(preTip){
+    console.log(preTip);
     let numOfGuests = document.getElementById("numOfGuests").value;
-    let preTip;
-    let tipAmount = document.querySelector("input:checked").value;
+    let tipAmount = document.querySelector("input[name=tip]:checked").value;
     let otherAmount = 0;
-    let total;
-    let tipTotal;
-    preTip = numOfTacos * 3;
- document.getElementById('preTip').innerHTML = "Pre-Total: $" + preTip;
+    let total = 0;
+    let tipTotal = 0;
+    
+ 
 
     if  (tipAmount == "other"){
         otherAmount = Number(document.getElementById('other').value);
@@ -57,5 +82,9 @@ function calcTip(){
 
 
 document.getElementById('calculate').addEventListener('click', function(e){
-    calcTip();
+    calcTip(preTip);
+})
+
+document.getElementById('order').addEventListener('click', function(e){
+    calcPreTotal();
 })
